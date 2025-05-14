865a876353bf916fcb01826c332af73470602fd2
Sebastian <31180501+sobonu@users.noreply.github.com>
2025-05-14 10:29:31 +0200

    v2 - edited age groups

--- FinalProject/csv_data_script.R
+++ FinalProject/csv_data_script.R
@@ -21,10 +21,10 @@ df_ages_years_all <- subset(df_ages_years_all, year >= 1800)
 # Creating age-groups: 
 
 df_ages_years_all <- df_ages_years_all %>%
-  mutate(age_group_1_25 = `1` + `2` + `3` + `4` + `5` + `6` + `7` + `8` + `9` + `10` + `11` + `12` + `13` + `14` + `15` + `16` + `17` + `18` + `19` + `20` + `21` + `22` + `23` + `24` + `25`) %>% 
-  mutate(age_group_26_35 = `26` + `27` + `28` + `29` + `30` + `31` + `32` + `33` + `34` + `35`) %>% 
-  mutate(age_group_36_50 = `36` + `37` + `38` + `39` + `40` + `41` + `42` + `43` + `44` + `45` + `46` + `48` + `49` + `50`) %>% 
+  mutate(age_group_1_21 = `1` + `2` + `3` + `4` + `5` + `6` + `7` + `8` + `9` + `10` + `11` + `12` + `13` + `14` + `15` + `16` + `17` + `18` + `19` + `20` + `21`) %>% 
+  mutate(age_group_22_30 = `22` + `23` + `24` + `25` + `26` + `27` + `28` + `29` + `30`) %>% 
+  mutate(age_group_31_50 =  `31` + `32` + `33` + `34` + `35` + `36` + `37` + `38` + `39` + `40` + `41` + `42` + `43` + `44` + `45` + `46` + `48` + `49` + `50`) %>% 
   mutate(age_group_51_99 = rowSums(df_ages_years_all[, 53:101]))
 
 
-df_ages_years_all <- df_ages_years_all[, c("year", "doc_count", "age_group_1_25", "age_group_26_35", "age_group_36_50", "age_group_51_99")]
+df_ages_years_all <- df_ages_years_all[, c("year", "doc_count", "age_group_1_21", "age_group_22_30", "age_group_31_50", "age_group_51_99")]
